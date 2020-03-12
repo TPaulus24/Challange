3fd50cc8e23179e83acd03aa238fc9cbfd7c0664
Tom Paulus <tom.paulus@student.rocvf.nl>
2020-03-12 10:59:44 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html class="wallpaper">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="web.css">
+    <title>Bit-Comments</title>
+</head>
+
+<body>
+    <div class="container">
+
+    </div>
+    <input type="text" class="textbox" placeholder="Type your comment">
+    <input type="button" value="Send" class="button">
+
+</body>
+
+</html>
